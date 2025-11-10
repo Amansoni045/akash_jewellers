2bffd787a7e6851d94fa5a792b66449cf08e330c
Aman Soni <amansoni@Amans-MacBook-Pro.local>
2025-11-10 23:20:38 +0530

    completed api/register/route.js

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,6 +1,44 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import { hashPassword } from "@/lib/auth";
 
 export async function POST(req) {
+  try {
+    const { name, email, password } = await req.json();
 
+    if (!name || !email || !password) {
+      return NextResponse.json({ error: "All fields required" }, { status: 400 });
+    }
+
+    const existingUser = await prisma.user.findUnique({ 
+        where:{ 
+            email 
+        } 
+    });
+    if (existingUser) {
+      return NextResponse.json({ error: "User already exists" }, { status: 400 });
+    }
+
+    const hashedPassword = hashPassword(password);
+
+    const newUser = await prisma.user.create({
+      data:{
+        name,
+        email, 
+        password: hashedPassword 
+    }
+    });
+
+    return NextResponse.json({
+      message: "User registered successfully",
+      user: { id: newUser.id, 
+        name: newUser.name, 
+        email: newUser.email 
+    },
+    });
+
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
 }

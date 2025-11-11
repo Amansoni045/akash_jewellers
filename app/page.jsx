85afb2099c08e13f4b93648d6e7c0a1efc0cdb87
Aman Soni <amansoni@Amans-MacBook-Pro.local>
2025-11-11 14:29:42 +0530

    frontend folder structure completed and converted js to jsx

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center text-center mt-20">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">Welcome to Akash Jewellers 💎</h1>
+    </div>
+  );
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,8 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function Dashboard() {
+    return (
+        <div>Navbar</div>
+    )
+}
